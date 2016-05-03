1144929a75165a1cc8048ef81e9f78f30224e9ad
Augusto <augustobw@spxxxx.serverpronto.com>
2016-05-03 09:46:58 -0400

    Adicionando css e js na index

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 <title>Monitoria</title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
 <meta charset="latin-1">
 </head>
 <body>
@@ -22,7 +24,10 @@
 
 
 
-
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
 </body>
 </html> 
