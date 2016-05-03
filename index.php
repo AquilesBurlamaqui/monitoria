4191a4c3d486a3df974ca6b2c2af961b1006e7ae
Aquiles Burlamaqui <aquilesburlamaqui@MacBook-Air-de-Aquiles.local>
2016-05-02 21:06:31 -0300

    iniciando cadastro de usuário

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <h1>Esta é a página do nosso sistema de monitoria</h1>
 <p>A Monitoria interliga monitores e alunos em busca de soluções para problemas</p>
 
+<a href="cadastrar_usuario.php">Cadastrar Usuário</a>
+<a href="cadastrar_problema.php">Cadastrar Problema</a>
+
+
+
+
 </body>
 </html> 
