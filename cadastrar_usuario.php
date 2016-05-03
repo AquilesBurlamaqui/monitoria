438a6936fb0c4279203ef277382f6ee3d06283c9
Aquiles Burlamaqui <aquilesburlamaqui@MacBook-Air-de-Aquiles.local>
2016-05-02 21:55:38 -0300

    criando arquivos para cadastrar

--- cadastrar_usuario.php
+++ cadastrar_usuario.php
@@ -1,1 +1,17 @@
-Cadastro de usuário
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cadastro de Usuário</title>
+</head>
+<body>
+
+<h1>Cadastro de Usuário</h1>
+<form method="POST" action="cadastrar.php">
+  Nome:<br>
+  <input type="text" name="name"><br>
+  E-mail:<br>
+  <input type="email" name="email"></br>
+ </form>
+
+</body>
+</html>
