4dc764cf33d6c68fcfae3501b56a36fa84909db3
Allan <allanbw@spxxxx.serverpronto.com>
2016-05-03 09:03:30 -0400

    ola

--- cadastroProblema.php
+++ cadastroProblema.php
@@ -9,16 +9,7 @@
     <link rel="stylesheet" type="text/css" href="css/index.css" />
 </head>
 <body>
-    <!-- Login Div -->
-    <!-- End of Login Div -->
-
-    <!-- Regiter Div -->
-  
-
-
-    <!-- Home Page Content-->
     <div id="homeContent" style="padding-top: 1rem;">
-        <!-- Main Page Content -->
         <!-- Nav Tabs -->
         <ul id="contentTabs" class="nav nav-tabs">
             <li role="presentation" class="active"><a href="index.html" aria-controls="home" role="tab" data-toggle="tab">  Home</a></li>
@@ -29,20 +20,13 @@
         <!-- Tab panes -->
             <!-- End of Home Content -->
         </div>
-        <!-- End of Tab Panes -->
-        <!-- Footer navbar navbar-inverse navbar-fixed-bottom-->
         <table class="homeFooter navbar-fixed-bottom">
             <tr>
                 <td align="right" class="copyrightContainer">© 2016 RQueiroz</td>
             </tr>
         </table>
-        <!-- End of Footer -->
-        <!-- End of Main Page's Content --> 
 
-        <!-- Alert Pop Up -->
-        <!-- Modal -->
-
- 		     <form method="post" action="form.php">
+ 	    <form method="post" action="form.php">
             <legend><h1>  Cadastre um problema:</h1></legend>
             <textarea class="input input-xlarge" name="ncode" id="icode" rows=20 cols=60></textarea>
             <input type="submit" value="Enviar">
