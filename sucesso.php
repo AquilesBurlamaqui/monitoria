83e60cb84a9eb0f8f76a5d9619f2670f7ac1f2e3
augusto pimenta <augustoppimenta@gmail.com>
2016-05-09 11:40:21 -0300

    Ultimas edições (não lembro o que fiz)

--- sucesso.php
+++ sucesso.php
@@ -1,3 +1,6 @@
+<div class="container">
+
+
 <?php
    // Start the session
    session_start();
@@ -11,17 +14,41 @@
 <head>
 <title>Monitoria</title>
 <meta charset="utf-8">
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bootstrap theme</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="listar_usuarios.php">Usuarios</a></li>
+            <li><a href="listar_problemas.php">Problemas</a></li>
+           </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 
-<h1>Esta é a página do nosso sistema de monitoria</h1>
-<p>Bem vindo, <?php echo $_SESSION["nome"]?></p>
+<div class="container theme-showcase" role="main">
+<div class="jumbotron">
+
+<h2>Bem vindo, <?php echo $_SESSION["nome"]?></h2>
 <p>A Monitoria interliga monitores e alunos em busca de soluções para problemas</p>
-<a href="listar_usuarios.php">Listar Usuário</a>
-<a href="listar_problemas.php">Listar Problemas</a>
-<a href="form_problema.php">Cadastrar Problema</a>
-<a href="logout.php">Logout</a>
+<a class="btn btn-primary" href="form_problema.php" role="button">Cadastrar problema</a>
 
+</div>
+<a href="logout.php">Logout</a>
+</br>
+</div>
 
 
 </body>
