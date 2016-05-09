3a7a0e21d2af9851843e7cf5a1c2de9d32f8d316
augusto pimenta <augustoppimenta@gmail.com>
2016-05-09 12:32:58 -0300

    Ajustando urls

--- menu.php
+++ menu.php
@@ -7,11 +7,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Monitando</a>
+          <a class="navbar-brand" href="sucesso.php">Monitando</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li>
+            <li><a href="sucesso.php">Home</a></li>
             <li><a href="listar_usuarios.php">Usuarios</a></li>
             <li><a href="listar_problemas.php">Problemas</a></li>
            </ul>
