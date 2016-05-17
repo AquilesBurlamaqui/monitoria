e507ad13c108673c44b1321b7fdd55e810511df8
Aquiles Burlamaqui <aquilesburlamaqui@MacBook-Air-de-Aquiles.local>
2016-05-16 22:15:39 -0300

    Organizando o codigo

--- sucesso.php
+++ sucesso.php
@@ -1,32 +1,28 @@
-<?php
-   // Start the session
-   session_start();
-   if($_SESSION["login"]!=true) 
-      echo "Usuário não logado";
-   else {
 
-?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Monitoria</title>
-<meta charset="utf-8">
-</head>
-<body>
+	<head>
+		<title>Monitoria</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	<?php
+	   //Inicia a sessão
+	   session_start();
 
-<h1>Esta é a página do nosso sistema de monitoria</h1>
-<p>Bem vindo, <?php echo $_SESSION["nome"]?></p>
-<p>A Monitoria interliga monitores e alunos em busca de soluções para problemas</p>
-<a href="listar_usuarios.php">Listar Usuário</a>
-<a href="listar_problemas.php">Listar Problemas</a>
-<a href="form_problema.php">Cadastrar Problema</a>
-<a href="logout.php">Logout</a>
-
-
-
-</body>
+	   if($_SESSION["login"]!=true) 
+	      echo "<h1>Usuário não logado</h1>";
+	   else {
+			?>
+			<h1>Esta é a página do nosso sistema de monitoria</h1>
+			<p>Bem vindo, <?php echo $_SESSION["nome"]?></p>
+			<p>A Monitoria interliga monitores e alunos em busca de soluções para problemas</p>
+			<a href="listar_usuarios.php">Listar Usuário</a>
+			<a href="listar_problemas.php">Listar Problemas</a>
+			<a href="form_problema.php">Cadastrar Problema</a>
+			<a href="logout.php">Logout</a>
+			<?php
+	   }
+	?>
+	</body>
 </html> 
-
-<?php
-  }
-?>
